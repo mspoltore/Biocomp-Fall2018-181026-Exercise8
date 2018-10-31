4663db8177fb18fb1174e1c37a18ff57a590ad72
Michael Spoltore <michaelspoltore@michaels-mbp.dhcp.nd.edu>
2018-10-31 15:10:11 -0400

    Some minor edits

--- exercise8.R
+++ exercise8.R
@@ -1,4 +1,4 @@
-# BioComputing Exercise 6
+# BioComputing Exercise 8
 
 rm(list=ls())
 setwd("~/Desktop/biocomp2018/exercise8/Biocomp-Fall2018-181026-Exercise8")
@@ -9,24 +9,26 @@ head(game)
 msuScore = 0
 uwScore = 0
 
+# Make empty column in table for cumulitive score
 game$cumScore=NA
+
 for(i in 1:nrow(game)) {
   if(game$team[i] == "MSU"){
     msuScore=msuScore + (game[i,3])
-    game[i,4]=msuScore
+    game[i,4]=msuScore #adds MSU's scores cumultively in rowif its an MSU row
   }else{
     uwScore=uwScore + (game[i,3])
-    game[i,4]=uwScore
+    game[i,4]=uwScore #adds UW's scores culutively in row if its a UW row
   }
 }
 
 print(msuScore)
 print(uwScore)
 a=ggplot(data=game,aes(x=time,y=cumScore))
-a+geom_line(aes(group=team, color=team))+xlab("Minutes")+ylab("Score")
+a+geom_line(aes(group=team, color=team))+xlab("Minutes")+ylab("Score")+theme_classic()
+
 
 # Question 2
-
 
 game=function(){
   n=sample(1:100,1)
@@ -42,7 +44,3 @@ game=function(){
   } 
   print("Correct!")
 }
-#ask user to make guess
-#while guess != random number
-#if/else 
-
